d1aa721be192f41b8f61ea7fbee2718d725660b2
Daniele Agueci <d.agueci1@gmail.com>
2018-03-09 01:20:20 -0500

    Use global constant for block size.

--- dist/snake.js
+++ dist/snake.js
@@ -5,7 +5,6 @@ var Snake = /** @class */ (function () {
     function Snake() {
         this.color = '#6fda6f';
         this.initialLength = 3;
-        this.blockSize = 10;
         this.coordinates = [];
         this.startingPosition = { x: 600 / 2, y: 400 / 2 };
     }
@@ -19,14 +18,14 @@ var Snake = /** @class */ (function () {
     Snake.prototype.Create = function () {
         this.coordinates = [];
         for (var i = 0; i < this.initialLength; i++) {
-            var newElement = { x: this.startingPosition.x, y: this.startingPosition.y + i * this.blockSize };
+            var newElement = { x: this.startingPosition.x, y: this.startingPosition.y + i * Constants.blockSize };
             this.coordinates.push(newElement);
             drawer_1.Drawer.DrawSquare(newElement, this.color);
         }
     };
     Snake.prototype.Move = function () {
         var tail = this.coordinates.pop();
-        this.coordinates.unshift({ x: this.coordinates[0].x, y: this.coordinates[0].y - this.blockSize });
+        this.coordinates.unshift({ x: this.coordinates[0].x, y: this.coordinates[0].y - Constants.blockSize });
         drawer_1.Drawer.DrawSquare(tail, Constants.canvasColor);
         drawer_1.Drawer.DrawSquare(this.coordinates[0], this.color);
     };
--- src/snake.ts
+++ src/snake.ts
@@ -5,7 +5,6 @@ export class Snake {
     private static _instance: Snake;
     private color: string = '#6fda6f';
     private initialLength: number = 3;
-    private blockSize: number = 10;
     private coordinates: Array<Coordinates> = [];
     private startingPosition: Coordinates = { x: 600 / 2, y: 400 / 2 };
 
@@ -18,7 +17,7 @@ export class Snake {
     Create(): void {
         this.coordinates = [];
         for (let i = 0; i < this.initialLength; i++) {
-            let newElement: Coordinates = { x: this.startingPosition.x, y: this.startingPosition.y + i * this.blockSize };
+            let newElement: Coordinates = { x: this.startingPosition.x, y: this.startingPosition.y + i * Constants.blockSize };
             this.coordinates.push(newElement);
             Drawer.DrawSquare(newElement, this.color);
         }
@@ -26,7 +25,7 @@ export class Snake {
     
     Move() {
         let tail = this.coordinates.pop() as Coordinates;
-        this.coordinates.unshift({ x: this.coordinates[0].x, y: this.coordinates[0].y - this.blockSize });
+        this.coordinates.unshift({ x: this.coordinates[0].x, y: this.coordinates[0].y - Constants.blockSize });
         
         Drawer.DrawSquare(tail, Constants.canvasColor);
         Drawer.DrawSquare(this.coordinates[0], this.color);
